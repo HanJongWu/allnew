5162ca470a1d09f323502385894bd1055cf8d3d0
HanJongWu <dogom77@naver.com>
2023-04-04 19:24:29 +0900

    atlas_4/4

--- node/atlas/atlas.js
+++ node/atlas/atlas.js
@@ -4,7 +4,9 @@ var data = JSON.stringify({
     "database": "test",
     "dataSource": "Cluster0",
     "projection": {
-        "_id": 1
+        "_id": 0,
+        "id": 1,
+        "name": 1
     }
 });
             
