3e1f38b80b171d5bdc774e689315f6b907bea8d6
HanJongWu <dogom77@naver.com>
2023-04-07 15:28:41 +0900

    push_0407

--- oracle/node/dbConfig.js
+++
@@ -1,6 +0,0 @@
-/* oracle Database Info */
-module.exports = {
-    user: process.env.NODE_ORACLEDB_USER || "hr",
-    password: process.env.NODE_ORACLEDB_PASSWOR || "1234",
-    connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "192.168.1.202:1521/xe"
-}
