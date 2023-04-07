7af6cc526361aac240ecb434dd72bf39c16d9e9c
HanJongWu <dogom77@naver.com>
2023-04-07 16:18:40 +0900

    xcc

--- node/atlas/atlas.js
+++ node/atlas/atlas.js
@@ -1,22 +1,24 @@
 var axios = require('axios');
+
+const env = require('dotenv').config({ path: "../../.env"})
+
 var data = JSON.stringify({
     "collection": "testdb",
     "database": "test",
     "dataSource": "Cluster0",
     "projection": {
         "_id": 0,
-        "id": 1,
-        "name": 1
+        "name" : 1
     }
 });
             
 var config = {
     method: 'post',
-    url: 'https://us-west-2.aws.data.mongodb-api.com/app/data-lvmne/endpoint/data/v1/action/findOne',
+    url: 'https://us-west-2.aws.data.mongodb-api.com/app/data-nymnn/endpoint/data/v1/action/findOne',
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Request-Headers': '*',
-      'api-key': ,
+      'api-key': process.env.ATLAS_API,
     },
     data: data
 };
