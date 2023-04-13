193db7b741a4009f13e28764d348928113027599
HanJongWu <dogom77@naver.com>
2023-04-13 11:44:45 +0900

    04_13_respush

--- node/auth/routes/main.js
+++ node/auth/routes/main.js
@@ -18,10 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/hello", (req, res) => {
-  res.send("Hello World~!!");
+  res.send("관리지 접속중입니다.");
 });
 
-// login
 app.post("/login", (req, res) => {
   const { id, pw } = req.body;
   const result = connection.query(
@@ -40,29 +39,51 @@ app.post("/login", (req, res) => {
   }
 });
 
-// register
 app.post("/register", (req, res) => {
   const { id, pw } = req.body;
-  const result = connection.query("insert into user values (?, ?)", [id, pw]);
-  console.log(result);
-  res.redirect("/");
+  if (id == "") {
+    res.redirect("register.html");
+  } else {
+    let result = connection.query("select * from user where userid=?", [id]);
+    if (result.length > 0) {
+      res.writeHead(200);
+      var template = `
+        <!doctype html>
+        <html>
+        <head>
+            <title>Error</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <div>
+              <h3 style="margin-left: 30px">Register Failed</h3>
+              <h4 style="margin-left: 30px">이미 존재하는 아이디입니다.<h4>
+              <a href="register.html" style="margin-left: 30px">다시 시도하기</a>
+            <div>
+        <body>
+        </html>
+       `;
+      res.end(template);
+    } else {
+      result = connection.query("insert into user values (?,?)", [id, pw]);
+      console.log(result);
+      res.redirect("/");
+    }
+  }
 });
 
-// request O, query X
 app.get("/select", (req, res) => {
   const result = connection.query("select * from user");
   console.log(result);
   res.send(result);
 });
 
-// request O, query X
 app.post("/select", (req, res) => {
   const result = connection.query("select * from user");
   console.log(result);
   res.send(result);
 });
 
-// request O, query O
 app.get("/selectQuery", (req, res) => {
   const id = req.query.id;
   const result = connection.query("select * from user where userid=?", [id]);
@@ -70,16 +91,13 @@ app.get("/selectQuery", (req, res) => {
   res.send(result);
 });
 
-// request O, query O
 app.post("/selectQuery", (req, res) => {
   const id = req.body.id;
-  // console.log(req.body);
   const result = connection.query("select * from user where userid=?", [id]);
   console.log(result);
   res.send(result);
 });
 
-// request O, query O
 app.post("/insert", (req, res) => {
   const { id, pw } = req.body;
   const result = connection.query("insert into user values (?, ?)", [id, pw]);
@@ -87,7 +105,6 @@ app.post("/insert", (req, res) => {
   res.redirect("/selectQuery?id=" + req.body.id);
 });
 
-// request O, query O
 app.post("/update", (req, res) => {
   const { id, pw } = req.body;
   const result = connection.query("update user set passwd=? where userid=?", [
@@ -98,7 +115,6 @@ app.post("/update", (req, res) => {
   res.redirect("/selectQuery?id=" + req.body.id);
 });
 
-// request O, query O
 app.post("/delete", (req, res) => {
   const id = req.body.id;
   const result = connection.query("delete from user where userid=?", [id]);
