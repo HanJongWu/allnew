826c2147daf611133fefa5ccd082dceaf195467a
HanJongWu <dogom77@naver.com>
2023-05-16 18:25:14 +0900

    5_16_python_push

---
+++ python/pythonDataProcess/p_490_xmlEx.py
@@ -0,0 +1,18 @@
+from xml.etree.ElementTree import parse
+tree = parse('xmlEx_03.xml')
+myroot = tree.getroot()
+print(type(myroot))
+print('-' * 40)
+
+familes = myroot.findall('가족')
+print(type(familes))
+print('-' * 40)
+
+for onefamily in familes:
+    for onesaram in onefamily:
+        if len(onesaram) >= 1:
+            print(onesaram[0].text)
+        else:
+            print(onesaram.attrib['이름'])
+    print('-' * 40)
+print('finished')
