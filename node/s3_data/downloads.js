44f4752d21691ad69c0a44df666a0c67488048eb
HanJongWu <dogom77@naver.com>
2023-06-15 02:29:05 +0000

    6_15_push

---
+++ node/s3_data/downloads.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const env = require('dotenv').config({ path: "../.env"});
+
+const AWS = require('aws-sdk');
+const ID = process.env.ID;
+const SECRET = process.env.SECRET;
+const BUCKET_NAME = 'han-2075';
+const MYREGION = 'ap-northeast-2'
+const s3 = new AWS.S3({accessKeyID : ID, secretAccessKEy: SECRET, region: MYREGION});
+
+const downloadFile = (fileName) => {
+    const params = {
+        Bucket: BUCKET_NAME,
+        Key : 'axios.png',
+    };
+    s3.getObject(params, function(err, data) {
+        if (err) { throw err; }
+        fs.writeFileSync(fileName, data.Body)
+    });
+};
+
+downloadFile('axios.png');
