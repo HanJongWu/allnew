9e3536aa6c78227c1604888a20f66a3a98134140
HanJongWu <dogom77@naver.com>
2023-07-21 06:34:35 +0000

    Resolved conflicts

--- docker/node-docker/server.js
+++ docker/node-docker/server.js
@@ -7,16 +7,8 @@ const HOST='0.0.0.0'
 const PORT='8000'
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
     res.send('Hello World \n');
-=======
-	res.send('Hello World \n');
->>>>>>> de95fd84f296b27228e7f62bd03f37e0da494ece
 });
 
 app.listen(PORT, HOST);
 console.log(`Server running at http://${HOST}:${PORT}/`);
-<<<<<<< HEAD
-=======
-
->>>>>>> de95fd84f296b27228e7f62bd03f37e0da494ece
--- docker/nodejs/.exrc
+++ docker/nodejs/.exrc
@@ -1,5 +1,1 @@
 set nu ai sm
-<<<<<<< HEAD
-
-=======
->>>>>>> de95fd84f296b27228e7f62bd03f37e0da494ece
